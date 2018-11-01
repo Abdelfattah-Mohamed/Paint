8c0f1e0f7668f373a6cc5ba5761be015346beb72
Abdelfattah Mohamed <abdelftahmohamed30@gmail.com>
2018-11-01 15:45:17 +0200

    First commit

---
+++ src/eg/edu/alexu/csd/oop/draw/cs23_cs24/Main_shape.java
@@ -0,0 +1,71 @@
+package eg.edu.alexu.csd.oop.draw.cs23_cs24;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.util.Map;
+
+import eg.edu.alexu.csd.oop.draw.Shape;
+
+public class Main_shape implements Shape {
+	
+	
+	public Object clone() throws CloneNotSupportedException {
+		return null;
+	}
+
+	@Override
+	public void setPosition(Point position) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Point getPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setProperties(Map<String, Double> properties) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Map<String, Double> getProperties() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setColor(Color color) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Color getColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setFillColor(Color color) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Color getFillColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void draw(Graphics canvas) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
